b9f24d8655d7b1828d947402968d3056052892c8
weidaoru <1034403136@qq.com>
2019-05-22 11:21:01 +0800

    update menu,menuchild mapper.xml

--- src/main/java/com/fushan/controller/menu/menuController.java
+++ src/main/java/com/fushan/controller/menu/menuController.java
@@ -1,15 +1,21 @@
 package com.fushan.controller.menu;
 
+import com.fushan.common.util.DataGrid;
+import com.fushan.service.menu.MenuInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class menuController {
+    @Resource
+    MenuInfoService menuInfoService;
     @RequestMapping("/menu/menuList")
-    public String menuList(Model model, HttpServletRequest request)throws Exception{
+    public String menuList(Model model, HttpServletRequest request, DataGrid dataGrid)throws Exception{
+        model.addAttribute("page",menuInfoService.pagedQuery(dataGrid));
         return "views/menu/menuList";
     }
 }
