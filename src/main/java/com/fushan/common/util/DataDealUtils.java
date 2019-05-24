e685a22ff00a720bfaf2d2524173a8fde2de3c73
weidaoru1 <1034403136@qq.com>
2019-05-24 22:53:36 +0800

    update to git

--- src/main/java/com/fushan/common/util/DataDealUtils.java
+++ src/main/java/com/fushan/common/util/DataDealUtils.java
@@ -15,48 +15,50 @@ public class DataDealUtils {
         if(newData != null && oldData != null){
             if (!newData.getCustomerName().equals(oldData.getCustomerName())){
                 paymentRecord = new PaymentRecord();
-                paymentRecord.setCustomerName(newData.getCustomerName());
             }
             if (!newData.getContact().equals(oldData.getContact())){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setContact(newData.getContact());
             }
             if (!newData.getPayee().equals(oldData.getPayee())){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setPayee(newData.getPayee());
             }
             if (!String.valueOf(newData.getAmount()).equals(String.valueOf(oldData.getAmount()))){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setAmount(newData.getAmount());
             }
             if (newData.getType() != oldData.getType()){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setType(newData.getType());
             }
             if (!DateUtils.dateToStr(newData.getPaymentTime()).equals(DateUtils.dateToStr(oldData.getPaymentTime()))){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setPaymentTime(newData.getPaymentTime());
             }
             if (!newData.getDetailsDes().equals(oldData.getDetailsDes())){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
-                paymentRecord.setDetailsDes(newData.getDetailsDes());
             }
             if (!newData.getRemark().equals(oldData.getRemark())){
                 if (paymentRecord == null){
                     paymentRecord = new PaymentRecord();
                 }
+            }
+            if (paymentRecord != null){
+                paymentRecord.setCustomerName(newData.getCustomerName());
+                paymentRecord.setContact(newData.getContact());
+                paymentRecord.setPayee(newData.getPayee());
+                paymentRecord.setAmount(newData.getAmount());
+                paymentRecord.setType(newData.getType());
+                paymentRecord.setPaymentTime(newData.getPaymentTime());
+                paymentRecord.setDetailsDes(newData.getDetailsDes());
                 paymentRecord.setRemark(newData.getRemark());
             }
         }
